336c60fadf7086b1c7321292d8f5846f02704d6e
Alejandro E. Rendon <alejorendon@utp.edu.co>
2017-02-28 14:36:21 -0500

    Finished

--- src/child_process/index.js
+++ src/child_process/index.js
@@ -1,6 +1,6 @@
 const child_process = require('child_process');
 
-child_process.exec('cat /etc/passwd', (err, stdout, stdin) => {
+child_process.exec('cat /etc/passwd', function(err, stdout, stdin) {
   if (err) console.log(err);
   else if (stdout) console.log(stdout);
   else if (stdin) console.log(stdin);
